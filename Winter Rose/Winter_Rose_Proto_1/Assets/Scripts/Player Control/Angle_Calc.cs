3ba948642d35a81b42e4205f4193907ae7ba8de4
David Mirabile <u0878838@ad.utah.edu>
2019-02-21 12:29:37 -0700

    Added the project to the repository

---
+++ Winter Rose/Winter_Rose_Proto_1/Assets/Scripts/Player Control/Angle_Calc.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Angle_Calc : MonoBehaviour
+{
+    private Vector3 lastFwd;
+    private float curAngleX = 0;
+
+
+    void Start()
+    {
+        lastFwd = transform.forward;
+    }
+
+    void Update()
+    {
+
+        Vector3 curFwd = transform.forward;
+        // measure the angle rotated since last frame:
+        float ang = Vector3.Angle(curFwd, lastFwd);
+        if (ang > 0.01)
+        { // if rotated a significant angle...
+          // fix angle sign...
+            if (Vector3.Cross(curFwd, lastFwd).x < 0) ang = -ang;
+            curAngleX += ang; // accumulate in curAngleX...
+            lastFwd = curFwd; // and update lastFwd
+        }
+
+        print(curAngleX);
+    }
+}
---
+++ Winter Rose/Winter_Rose_Proto_1/Assets/Scripts/Prefabs/SnowTilePrefab.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowTilePrefab : MonoBehaviour
+{
+  
+    public GameObject SnowPrefab;
+    public GameObject Snowball;
+    public Vector3 pos;
+    public Vector3 startPos;
+    
+
+    private void Update()
+    {
+        //Snowball = GameObject.Find("Snowball");
+        //pos = Snowball.transform.position;
+        //startPos = pos;
+        //pos.x -= 1;
+
+        /*
+        if (transform.hasChanged && transform.position.x >= startPos.x)
+        {
+            GameObject snow = (GameObject)Instantiate(SnowPrefab, pos, transform.rotation, null);
+        }
+        */
+        
+
+    }
+}
