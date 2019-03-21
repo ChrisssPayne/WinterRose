0699530ab7a0f42f8bbd574d5359b1b7fd5081c6
ChrisssPayne <aindel01@live.com>
2019-03-21 00:14:23 -0600

    Touchups

---
+++ Assets/Scripts/Prefabs/SeedPrefab.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedPrefab : MonoBehaviour
+{
+
+    public GameObject Icon;
+    SpriteRenderer seedRenderer;
+    Rigidbody2D rb;
+    public bool active;
+
+    void Start()
+    {
+
+        seedRenderer = GetComponent<SpriteRenderer>();
+        if (active)
+            seedRenderer.enabled = true;
+        else
+            seedRenderer.enabled = false;
+    }
+
+    private void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Snowball"))
+        {
+            if (active)
+            {
+                //Hide seed icon
+                Icon.SetActive(false);
+                seedRenderer.enabled = false;
+                active = false;
+            }
+            else
+            {
+                //Nothing
+            }
+        }
+    }
+}
